2f2ec442790cabf56eeacf21180d9cb84d64284e
Lee-Seungik <69029566+Lee-Seungik@users.noreply.github.com>
2022-03-29 20:11:41 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 int main() 
  {
-  Area A1, A2(2,1);
+		Area A1, A2(2,1);
   int temp;
   
   cout<< "Default Area" << endl; temp = A1.AreaCalculation();
   A1.DisplayArea(temp);
-  cout<< "Area when (2,1) is " \ "passed" << endl;
+ 
+  cout<< "Area when (2,1) is passed" << endl;
   temp = A2.AreaCalculation();
   A2.DisplayArea(temp);
   
+ 
+  A3.GetLength();
+  temp = A3.AreaCalculation();
+  cout<< "Calculation ";
+  A3.DisplayArea(temp);
+    
   return 0;
  }
